3b22fe548b52a142569b4a4af0d9af6249954547
gusmorkar <62334538+gusmorkar@users.noreply.github.com>
2023-03-13 14:20:24 +0200

    TEST
    Jsut a test

---
+++ Assets/TestVersion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*just testing the git commit*/
+public class TestVersion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
