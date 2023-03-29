a1c0b03426fc7aefaab7c655e082c799eab67c84
Samu Hämäläinen <samu.hamalainen@tuni.fi>
2023-03-29 15:09:26 +0300

    Made a Main Menu and a start button

---
+++ Assets/StartGameButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour
+{
+    public string gameStartScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameStartScene);
+    }
+}
