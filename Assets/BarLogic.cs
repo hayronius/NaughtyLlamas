c5d8b77a500546f31e4177014bede86542e99ddb
Samu Hämäläinen <samu.hamalainen@tuni.fi>
2023-03-29 14:09:03 +0300

    Added the 4 bars, choosing a backstory affects their values

--- Assets/BarLogic.cs
+++ Assets/BarLogic.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Fungus;
 
 public class BarLogic : MonoBehaviour
 {
     public TextMeshProUGUI valueText;
     public Slider slider;
-    
+    public VariableReference var;
+
     public void OnBarChanged(float value)
     {
         valueText.text = value.ToString();
@@ -17,5 +19,10 @@ public class BarLogic : MonoBehaviour
     public void UpdateBar(int change)
     {
         slider.value = change;
+    }
+
+    void Update()
+    {
+        UpdateBar(var.Get<int>());
     }
 }
