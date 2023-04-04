d07c07c03d2c0cc99033dacdcc048d7a4f3ad918
Samu Hämäläinen <samu.hamalainen@tuni.fi>
2023-04-04 14:31:05 +0300

    Added a couple of saves and made the initial pop up window for the Decision Tree

---
+++ Assets/DecisionTreeButtonScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionTreeButtonScript : MonoBehaviour
+{
+    public GameObject DecisionTree;
+    public void ShowDecisionTree()
+    {
+        DecisionTree.SetActive(true);
+    }
+}
---
+++ Assets/TreeNodeScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Fungus;
+
+public class TreeNodeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var saveManager = FungusManager.Instance.SaveManager;
+        //saveManager;
+    }
+
+    
+}
