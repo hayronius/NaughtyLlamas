681095afefb1455d559c5d977efaede697164480
gusmorkar <62334538+gusmorkar@users.noreply.github.com>
2023-04-19 16:16:50 +0300

    audio button

--- Assets/MuteAudioButtonScript.cs
+++ Assets/MuteAudioButtonScript.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class MuteAudioButtonScript : MonoBehaviour
 {
     public bool audioOn = true;
+
+    private GameObject goAudioOn;
+    private GameObject goAudioOff;
+
+    public void Start()
+    {
+        goAudioOn = this.gameObject.transform.GetChild(0).gameObject;
+        goAudioOff = this.gameObject.transform.GetChild(1).gameObject;
+    }
+
     public void ToggleAudio()
     {
         audioOn = !audioOn;
         if (!audioOn)
         {
             AudioListener.volume = 0;
+            goAudioOn.SetActive(false);
+            goAudioOff.SetActive(true);
         }
         else
         {
             AudioListener.volume = 1;
+            goAudioOn.SetActive(true);
+            goAudioOff.SetActive(false);
         }
     }
-
 }
